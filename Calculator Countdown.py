53f82988a36e3ae33e1279830bd24e212601411b
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-05-31 23:13:40 +0200

    calculator

--- Calculator Countdown.py
+++ Calculator Countdown.py
@@ -1,4 +1,34 @@
+import time
+
+def countdown(d):
+   while d:
+      mins, secs = divmod(d, 60)
+      timer = '{:02d}:{:02d}'. format(mins,secs)
+      print(timer, end="/r")
+      time.sleep(1)
+      d -= 1
+   
+  print('Fire in the hole!!')
+    
+
+d = input("Enter the time in seconds: ")
+
+countdown(int(d))
 
 
+import time
 
-i
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
+    print('Fire in the hole!!')
+
+t = input("Enter the time in seconds: ")
+countdown(int(t))
+  
+  
