c9c7b71cbcced2e970754ad0f6f5377b12e550fb
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-05-31 10:32:56 +0200

    odd/even

---
+++ Odd:Even.py
@@ -0,0 +1,10 @@
+#Python Program to check if a numnber is odd or even
+
+num = int(input("Enter a number: "))
+if (num  % 2) == 0:
+    print("{0} is even".format(num))
+else:
+    print("{0} is Odd".format(num))
+    
+  
+  
