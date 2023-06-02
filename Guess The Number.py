dae6a31e9d261443600108ca2b0c8162c945628a
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-06-02 20:25:14 +0200

    guess

---
+++ Guess The Number.py
@@ -0,0 +1,26 @@
+#function for the guess the number game
+from random import randint
+
+
+num = randint(0,10)
+guess = None
+
+def guessnum():
+    global guess
+    while guess != num:
+        guess = input("guess a number between 0 and 10: ")
+      
+        if guess == ' ':
+            print("Please enter a valued number.")
+            continue
+       
+        guess = int(guess)
+
+        if guess == num:
+              print("congratulations! you won")
+              break
+        else:
+             print("nope, sorry. try again!")
+             
+guessnum()
+
