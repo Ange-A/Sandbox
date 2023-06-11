237ad4f9f51f094938ea1377e23c13577a231e62
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-06-11 13:19:56 +0200

    reverse

--- Reversed_String.py
+++ Reversed_String.py
@@ -1,13 +1,11 @@
 #function to reverse a string
 
-def reversed(string):
+def reverse_string(string):
     reversed_str = string[::-1]
     return reversed_str
-  
-  
-input_string = input("what string do you want to reverse?: "))
-reversed_string =  reversed_string(input_string)
-print("the string you reversed is: ",reversed_string)
-  
+
+input_string = input("Enter a string: ")
+reversed_string = reverse_string(input_string)
+print("Reversed string:", reversed_string)
   
   
